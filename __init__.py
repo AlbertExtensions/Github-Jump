fdec8b3c79b55c0d8d224c4d6f2b2a3d28ad6a4e
Bharat <bharatkalluri@protonmail.com>
2021-05-26 04:33:39 +0530

    Support new Albert API

--- __init__.py
+++ __init__.py
@@ -3,58 +3,59 @@ import time
 
 import json
 import os
-from albertv0 import *
-from difflib import SequenceMatcher as SM
+from typing import Dict, List, Optional
+from albert import *
+from difflib import SequenceMatcher
 from github import Github
 from os import path
 from pathlib import Path
 
-__iid__ = "PythonInterface/v0.1"
-__prettyname__ = "Github Jump"
-__version__ = "0.1"
-__trigger__ = "gj"
+__title__ = "Github Jump"
+__version__ = "0.0.1"
+__triggers__ = "gj"
 __author__ = "Bharat kalluri"
-__dependencies__ = ["pygithub", "boltons"]
+__py_deps__ = ["pygithub", "boltons"]
+
+from github.AuthenticatedUser import AuthenticatedUser
+from github.Repository import Repository
 
 icon_path = "{}/icons/{}.png".format(path.dirname(__file__), "repo")
-CONFIG_PATH = os.path.join(configLocation(), 'gh_token')
-CACHE_PATH = os.path.join(dataLocation(), 'gh_cache')
+CONFIG_PATH = os.path.join(configLocation(), "gh_token")
+CACHE_PATH = os.path.join(dataLocation(), "gh_cache")
 
 
-def get_token():
-    gh_token_file = Path(CONFIG_PATH)
+def get_token() -> str:
+    gh_token_file: Path = Path(CONFIG_PATH)
     if gh_token_file.is_file():
         return gh_token_file.read_text().strip()
     else:
         raise FileNotFoundError
 
 
-def save_token(github_token: str):
-    with open(CONFIG_PATH, 'w') as f:
+def save_token(github_token: str) -> None:
+    with open(CONFIG_PATH, "w") as f:
         f.write(github_token)
 
 
-def get_repos(github_token, cache_override: bool = False):
-    gh_cache_file = Path(CACHE_PATH)
+def get_repos(github_token: str, cache_override: bool = False) -> list[dict[str, str]]:
+    gh_cache_file: Path = Path(CACHE_PATH)
     if cache_override is False and gh_cache_file.is_file():
         print("Getting from cache")
         repo_list = json.loads(gh_cache_file.read_text())
     else:
         print("Cache empty, getting repos")
-        repo_list = []
-        g = Github(github_token)
-        user_data = g.get_user()
-        starred = user_data.get_starred()
-        personal_repos = user_data.get_repos()
-        for repo in personal_repos:
-            repo_list.append(repo)
-        for repo in starred:
-            repo_list.append(repo)
-        repo_list = [{
-            'name': repo.name,
-            'description': repo.description,
-            'html_url': repo.html_url
-        } for repo in repo_list]
+        g: Github = Github(github_token)
+        user_data: AuthenticatedUser = g.get_user()
+        starred: list[Repository] = list(user_data.get_starred())
+        personal_repos: list[Repository] = list(user_data.get_repos())
+        repo_list: list[dict[str, str]] = [
+            {
+                "name": repo.name,
+                "description": repo.description,
+                "html_url": repo.html_url,
+            }
+            for repo in (starred + personal_repos)
+        ]
         gh_cache_file.write_text(json.dumps(repo_list))
     return repo_list
 
@@ -66,8 +67,45 @@ def list_safe_get(arr: list, position: int):
         return None
 
 
-def handleQuery(query):
+def get_search_results(input_query: str, github_token: str) -> List[Item]:
+    repo_list: List[Dict] = get_repos(github_token)
+    cleaned_query = str(input_query).replace("#", "").replace("!", "")
+    repos_with_match_percentage = [
+        {
+            "match_percentage": SequenceMatcher(
+                lambda x: x in [" ", "-"],
+                cleaned_query,
+                repo["name"].lower(),
+            ).ratio(),
+            "repo_info": repo,
+        }
+        for repo in repo_list
+    ]
+    repos_with_match_percentage.sort(key=lambda x: x.get('match_percentage'), reverse=True)
+
     results = []
+    for repo_with_match_percentage in repos_with_match_percentage:
+        repo = repo_with_match_percentage.get('repo_info')
+        repo_name = repo.get('name', '')
+        repo_url = repo.get("html_url", '')
+        repo_description: Optional[str] = repo.get('description')
+        if "!" in str(input_query):
+            repo_url = repo_url + "/issues"
+        elif "#" in str(input_query):
+            repo_url = repo_url + "/pulls"
+        results.append(
+            Item(
+                id=__title__,
+                icon=icon_path,
+                text=repo_name,
+                subtext=repo_description if repo_description else '',
+                actions=[UrlAction("Open in Github", repo_url)],
+            )
+        )
+    return results
+
+
+def handleQuery(query):
 
     if query.isTriggered and query.string.strip():
 
@@ -76,26 +114,21 @@ def handleQuery(query):
         if not query.isValid:
             return
 
+        query.disableSort()
+
         input_query = query.string.strip()
         input_query_arr = input_query.split(" ")
-
-        item = Item(
-            id=__prettyname__,
-            icon=icon_path,
-            completion=query.rawString,
-            text=__prettyname__,
-            actions=[]
-        )
 
         if list_safe_get(input_query_arr, 0) == "token":
             gh_token = list_safe_get(input_query_arr, 1)
             return Item(
-                id=__prettyname__,
+                id=__title__,
                 icon=icon_path,
                 text="Save token?",
                 subtext=f"Github token: {gh_token} will be saved on enter",
-                actions=[FuncAction(text="Save token",
-                                    callable=lambda: save_token(gh_token))]
+                actions=[
+                    FuncAction(text="Save token", callable=lambda: save_token(gh_token))
+                ],
             )
 
         # Require a token to start
@@ -103,46 +136,37 @@ def handleQuery(query):
             github_token = get_token()
         except FileNotFoundError:
             return Item(
-                id=__prettyname__,
+                id=__title__,
                 icon=icon_path,
                 text="Token needed",
-                subtext="Please give a token by giving gj token [your token]")
+                subtext="Please give a token by giving gj token [your token]",
+            )
 
-        if list_safe_get(input_query_arr, 0) == 'cache' and list_safe_get(input_query_arr, 1) == 'refresh':
+        if (
+                list_safe_get(input_query_arr, 0) == "cache"
+                and list_safe_get(input_query_arr, 1) == "refresh"
+        ):
             print("Force refresh cache")
             return Item(
-                id=__prettyname__,
+                id=__title__,
                 icon=icon_path,
                 text="Refresh cache?",
                 subtext="Cache will be refreshed on enter",
-                actions=[FuncAction(text="Refresh cache",
-                                    callable=lambda: get_repos(github_token, cache_override=True))]
+                actions=[
+                    FuncAction(
+                        text="Refresh cache",
+                        callable=lambda: get_repos(github_token, cache_override=True),
+                    )
+                ],
             )
 
-        if len(query.string) >= 1:
-            repo_list = get_repos(github_token)
-            match_list = []
-            cleaned_query = str(input_query).replace('#', '').replace('!', '')
-            for repo in repo_list:
-                match_list.append([repo, SM(
-                    lambda x: x in [" ", "-"],
-                    cleaned_query, repo['name'].lower()).ratio()
-                                   ])
-            match_list.sort(key=lambda x: x[1], reverse=True)
-            for repo, ratio in match_list:
-                repo_url = repo['html_url']
-                if "!" in str(input_query):
-                    repo_url = repo_url + "/issues"
-                elif "#" in str(input_query):
-                    repo_url = repo_url + "/pulls"
-                results.append(Item(
-                    id=__prettyname__,
-                    icon=icon_path,
-                    text=repo['name'],
-                    subtext=repo['description'] if repo['description'] else "",
-                    actions=[UrlAction("Open in Github", repo_url)])
-                )
-        else:
-            item.subtext = "Jump to repo in Github! Enter more than 2 Characters to begin search."
-            return item
-    return results
+        if not query.string.strip():
+            return Item(
+                id=__title__,
+                icon=icon_path,
+                text="Github Jump",
+                subtext="Jump to repo in Github! Enter more than 2 Characters to begin search.",
+            )
+
+        return get_search_results(query.string, github_token)
+
